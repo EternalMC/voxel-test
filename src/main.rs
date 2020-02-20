d69f2179bab0c239755365a22fbc7b76d1f264b4
EternalMC <55027815+EternalMC@users.noreply.github.com>
2020-02-20 14:29:38 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -111,7 +111,7 @@ fn main() {
     ).unwrap();
     let mut events_loop = glutin::EventsLoop::new();
     let mut application = Application::new(&events_loop);
-    application.display.gl_window().set_cursor(glutin::MouseCursor::Grab).expect("couldn't grab cursor");
+    application.display.gl_window().set_cursor(glium::glutin::::MouseCursor::Grab).expect("couldn't grab cursor");
     let mut cursor_grabbed = true;
 
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TriangleStrip);
